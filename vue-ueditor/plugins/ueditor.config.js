33c4d129303339cad9ec86c07f8ecd1406409a86
jiangyangjun <yogi.jiang@hotmail.com>
2017-03-23 20:26:25 +0800

    remove serverUrl

--- vue-ueditor/plugins/ueditor.config.js
+++ vue-ueditor/plugins/ueditor.config.js
@@ -29,9 +29,6 @@
         //为编辑器实例添加一个路径，这个不能被注释
         UEDITOR_HOME_URL: URL
 
-        // 服务器统一请求接口路径
-        , serverUrl: URL + "static/ueditor/nodejs/config.json"
-
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [
             [
